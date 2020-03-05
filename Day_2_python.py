ebf497726cd2f387cf85d41e8ddad55c798cbdc9
abkumars77 <59677113+abkumars77@users.noreply.github.com>
2020-03-05 21:58:26 +0530

    Add files via upload

---
+++ Day_2_python.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 5
+y = 7
+
+z = x + y
+
+
+# In[2]:
+
+
+print(z)
+
+
+# In[3]:
+
+
+first_name = 'kumar'
+second_name = 'swamy'
+print(first_name)
+
+
+# In[5]:
+
+
+print(first_name.title())
+
+
+# In[33]:
+
+
+print(f"My first name {first_name} and surname is {second_name} ")
+
+
+# In[35]:
+
+
+print(second_name.upper())
+
+
+# In[37]:
+
+
+print(first_name.lower())
+
+
+# In[38]:
+
+
+print(f"my fullname is {first_name} {second_name}")
+
+
+# In[ ]:
+
+
+
+
